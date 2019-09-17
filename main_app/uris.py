3ca75e7248c8dce3efc61aab15ed86ea48189e40
Jack Ground <jaground@gmail.com>
2019-09-17 15:13:35 -0500

    fixes .env files

--- main_app/uris.py
+++ main_app/uris.py
@@ -5,5 +5,5 @@ from . import spotify_views
 urlpatterns = [
   path('link-user/', spotify_views.link_user, name = 'fetch-code'),
   path('receive-code/', spotify_views.handle_code),
-  # path('spotify/receive-token/', spotify-views.handle_token),
+  path('renew-token/', spotify_views.renew_token),
 ]

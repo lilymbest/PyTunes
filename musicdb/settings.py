85ca85aca91c10496b407270969483e6120deb16
Lily Best <lily.m.best@gmail.com>
2019-09-18 12:15:26 -0500

    Config deployment

--- musicdb/settings.py
+++ musicdb/settings.py
@@ -14,8 +14,7 @@ import os
 import environ
 environ.Env()
 environ.Env.read_env()
-import django_heroku
-django_heroku.settings(locals())
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -128,3 +127,6 @@ LOGIN_REDIRECT_URL = '/spotify/renew-token/'
 
 LOGOUT_REDIRECT_URL = '/'
 SIGNUP_REDIRECT_URL = 'spotify/link-user/'
+
+import django_heroku
+django_heroku.settings(locals())

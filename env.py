54663042586f99ead716e6f6d15e97fc32c3d331
lilymbest <52476585+lilymbest@users.noreply.github.com>
2019-09-18 12:22:02 -0500

    stop

--- env.py
+++
@@ -1,4 +0,0 @@
-SPOTIFY_CLIENT_ID = '6db6c797581140b5adf37524b2cc123e'
-SPOTIFY_CLIENT_SECRET = 'db873aab3ad34ea6ba7cb2fa651bee85'
-SPOTIFY_REDIRECT_URI_ENCODED = 'http%3A%2F%2Flocalhost%3A8000%2Fspotify%2Freceive-code%2F'
-SPOTIFY_REDIRECT_URI = 'http://localhost:8000/spotify/receive-code/'

6a35ce64d7dd7f2dcf521c2aeed24e9c9a66f65e
Lily Best <lily.m.best@gmail.com>
2019-09-19 15:37:38 -0500

    environ troubleshoot

--- musicdb/settings.py
+++ musicdb/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import os
+
 import environ
+import os
 environ.Env()
 environ.Env.read_env()
 
